1dae8127bf204f8dc05dc05e7a2d801f5baf4ae6
Mohamed Essam <mohammedessamomar@gmail.com>
2021-09-01 17:25:46 +0200

    Add files via upload

--- Pointers/main.c
+++ Pointers/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void swap(int *x,int *y);
+
+int main()
+{
+
+     int x=1;
+    int y=2;
+
+    printf("The value before swapping is :%d %d\n",x,y);
+
+    swap(&x,&y);
+
+    printf("The value after the  swapping function is :%d %d\n",x,y);
+
+}
+
+void swap(int *x,int *y){
+
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+
+
+}
